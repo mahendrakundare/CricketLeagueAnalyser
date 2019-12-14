461fd98860aa27e51b5b27ae8c4b3b72a642216b
mahendrakundare <mr.kundare@gmail.com>
2019-12-14 13:21:00 +0530

    Refactored Code and Added external jar file for reading CSV Files

--- src/main/java/com/bridgelabz/cricketleague/CricketAnalyser.java
+++ src/main/java/com/bridgelabz/cricketleague/CricketAnalyser.java
@@ -1,31 +1,34 @@
 package com.bridgelabz.cricketleague;
 
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
+import com.bridgelabz.csvbuilder.CSVBuilderException;
+import com.bridgelabz.csvbuilder.CSVBuilderFactory;
+import com.bridgelabz.csvbuilder.ICSVBuilder;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.stream.StreamSupport;
 
 public class CricketAnalyser {
 
-    public int readData(String csvFilePath) {
-        int count=0;
-        try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
-            CsvToBean csvToBean = new CsvToBeanBuilder(reader)
-                    .withType(Batsman.class)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build();
-            Iterator<Batsman>batsmanIterator = csvToBean.iterator();
-            while (batsmanIterator.hasNext()) {
-                Batsman batsman = batsmanIterator.next();
-                count++;
-            }
-        } catch (IOException e) {
+    public int readData(String csvFilePath) throws IOException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+            Iterator<Batsman> csvFileterator = csvBuilder.getCSVFileIterator(reader, Batsman.class);
+            return this.getCount(csvFileterator);
+        } catch (CSVBuilderException e) {
             e.printStackTrace();
         }
-        return count;
+        return 0;
+    }
+
+    private int getCount(Iterator iterator) {
+        Iterable csvIterable = () -> iterator;
+        int numOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+        return numOfEateries;
     }
 }
+
+
--- src/test/java/com/bridgelabz/cricketleague/CricketAnalyserTest.java
+++ src/test/java/com/bridgelabz/cricketleague/CricketAnalyserTest.java
@@ -3,15 +3,18 @@ package com.bridgelabz.cricketleague;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class CricketAnalyserTest {
     private static final String IPL2019_RUNS_CSV_FILE_PATH = "/home/admin1/IdeaProjects/CricketLeagueAnalyser/src/test/resources/IPL2019FactsheetMostRuns.csv";
     private static final String IPL2019_WICKETES_CSV_FILE_PATH="/home/admin1/IdeaProjects/CricketLeagueAnalyser/src/test/resources/IPL2019FactsheetMostWkts.csv";
 
-
     @Test
     public void givenLeagueDataCSVFile_ShouldReturnExactCount() {
-        CricketAnalyser cricketAnalyser = new CricketAnalyser();
-        int totalRecord = cricketAnalyser.readData(IPL2019_RUNS_CSV_FILE_PATH);
-        Assert.assertEquals(101,totalRecord);
+        try {
+            CricketAnalyser cricketAnalyser = new CricketAnalyser();
+           int totalRecord = cricketAnalyser.readData(IPL2019_RUNS_CSV_FILE_PATH);
+            Assert.assertEquals(101,totalRecord);
+        } catch (IOException e) { }
     }
 }
