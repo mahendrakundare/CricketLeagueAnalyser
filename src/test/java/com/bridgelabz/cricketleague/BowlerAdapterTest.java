065296934da8b760cb658ebdf8ee82afddc569fd
mahendrakundare <mr.kundare@gmail.com>
2019-12-26 09:16:08 +0530

    Added Code and Test for mokito

--- src/test/java/com/bridgelabz/cricketleague/BowlerAdapterTest.java
+++ src/test/java/com/bridgelabz/cricketleague/BowlerAdapterTest.java
@@ -1,11 +1,10 @@
 package com.bridgelabz.cricketleague;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.Map;
 
-public class BatsmanAdapterTest {
+public class BowlerAdapterTest {
+
     private static final String IPL2019_RUNS_CSV_FILE_PATH = "/home/admin1/IdeaProjects/CricketLeagueAnalyser/src/test/resources/IPL2019FactsheetMostRuns.csv";
     private static final String IPL2019_WICKETES_CSV_FILE_PATH = "/home/admin1/IdeaProjects/CricketLeagueAnalyser/src/test/resources/IPL2019FactsheetMostWkts.csv";
     private static final String WRONG_FILE = "/home/admin1/IdeaProjects/CricketLeagueAnalyser/src/test/resources/IPL2019FactsheetMostWktsy.csv";
@@ -15,11 +14,10 @@ public class BatsmanAdapterTest {
     private static final String SAMPLE_BATSMAN_FILE ="/home/admin1/IdeaProjects/CricketLeagueAnalyser/src/test/resources/SampleIPLData.csv";
     private static final String SAMPLE_BOWLER_FILE="/home/admin1/IdeaProjects/CricketLeagueAnalyser/src/test/resources/sampleBowlerFile.csv";
 
-
     @Test
     public void givenBatsmanData_ReturnExactPlayerCount() {
         try {
-            BatsmanAdapter batsmanAdapter = new BatsmanAdapter();
+            BowlerAdapter batsmanAdapter = new BowlerAdapter();
             Map<String, CricketLeagueDAO> batsmanMap = batsmanAdapter.readData(SAMPLE_BATSMAN_FILE,SAMPLE_BOWLER_FILE);
             Assert.assertEquals(11,batsmanMap.size());
         } catch (CricketLeagueException e) { }
@@ -28,8 +26,8 @@ public class BatsmanAdapterTest {
     @Test
     public void givenLeagueCSVFile_IFIncorrect_ShouldThrowException() {
         try {
-            BatsmanAdapter batsmanAdapter = new BatsmanAdapter();
-            Map<String, CricketLeagueDAO> batsmanMap = batsmanAdapter.readData(WRONG_FILE);
+            BowlerAdapter bowlerAdapter = new BowlerAdapter();
+            Map<String, CricketLeagueDAO> batsmanMap = bowlerAdapter.readData(WRONG_FILE);
         } catch (CricketLeagueException e) {
             Assert.assertEquals(e.type, CricketLeagueException.ExceptionType.FILE_PROBLEM);
         }
@@ -38,8 +36,8 @@ public class BatsmanAdapterTest {
     @Test
     public void givenLeagueCSVFile_IfFileISEmpty_ShouldThrowException() {
         try {
-            BatsmanAdapter batsmanAdapter = new BatsmanAdapter();
-            batsmanAdapter.readData(EMPTY_FILE);
+            BowlerAdapter bowlerAdapter = new BowlerAdapter();
+            bowlerAdapter.readData(EMPTY_FILE);
         } catch (CricketLeagueException e) {
             Assert.assertEquals(e.type, CricketLeagueException.ExceptionType.FILE_PROBLEM);
         }
@@ -48,8 +46,8 @@ public class BatsmanAdapterTest {
     @Test
     public void givenLeagueCSVFile_IfFileHasInvalidDelimeter_ShouldThrowError() {
         try {
-            BatsmanAdapter batsmanAdapter = new BatsmanAdapter();
-            batsmanAdapter.readData(FILE_WITH_DELIMETER_PROBLEM);
+            BowlerAdapter bowlerAdapter = new BowlerAdapter();
+            bowlerAdapter.readData(FILE_WITH_DELIMETER_PROBLEM);
         } catch (CricketLeagueException e) {
             Assert.assertEquals(e.type, CricketLeagueException.ExceptionType.DELIMITER_OR_HEADER_PROBLEM);
         }
@@ -58,14 +56,10 @@ public class BatsmanAdapterTest {
     @Test
     public void givenLeagueCSVFile_IfFileHasInvalidHeader_ShouldThrowError() {
         try {
-            BatsmanAdapter batsmanAdapter = new BatsmanAdapter();
-            batsmanAdapter.readData(INVALID_HEADER_FILE);
+            BowlerAdapter bowlerAdapter = new BowlerAdapter();
+            bowlerAdapter.readData(INVALID_HEADER_FILE);
         } catch (CricketLeagueException e) {
             Assert.assertEquals(e.type, CricketLeagueException.ExceptionType.DELIMITER_OR_HEADER_PROBLEM);
         }
     }
-
-
-
-
 }
